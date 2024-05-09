6d1dda6217006967a548975b5311cf6a874883a6
Marc Mansour <marcm9387@gmail.com>
2024-05-09 22:18:55 +0200

    rails simple airbnb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -14,14 +14,37 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     @flat.save
-
     redirect_to flat_path(@flat)
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to @flat
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  def search
+    @flats = if params[:query].present?
+               Flat.where("name LIKE ?", "%#{params[:query]}%")  # Adjust this according to your search criteria
+             else
+               Flat.all  # or whatever default listing you want
+             end
   end
 
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :picture_url)
   end
 
   def set_flat
